d89f2af8116d2eda7af7d3879ed671db2f0b7396
ouidlut <35586351+ouidlut@users.noreply.github.com>
2018-07-13 14:25:32 +0900

    1
    開発済み

--- sidecomp/server/config.js
+++ sidecomp/server/config.js
@@ -3,7 +3,7 @@ const CONF = {
     rootPathname: '',
 
     // 微信小程序 App ID
-    appId: 'wx13ec741be3001633',
+    appId: '',
 
     // 微信小程序 App Secret
     appSecret: '',
@@ -21,7 +21,7 @@ const CONF = {
         port: 3306,
         user: 'root',
         db: 'cAuth',
-        pass: 'wx13ec741be3001633',
+        pass: '',
         char: 'utf8mb4'
     },
 
